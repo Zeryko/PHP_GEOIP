043b0e1db18a00e91a22a6e5ff440dd43b331787
Antoine <Antoinedblt@gmail.com>
2022-01-07 16:23:39 +0100

    start part 2

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php 
 
-    function geolocalisation(){
-        echo "test";
+
+    function getClientIP(){
+        $cli_ip = $_SERVER['REMOTE_ADDR'];
+        return $cli_ip;
     }
+    
+    function transformIP($ip){
+        $ipParse = explode(".",$ip);
+        $ipCalc = $ipParse[3] + $ipParse[2] * 256 + $ipParse[1] * 256 * 256 + $ipParse[0] * 256 * 256 * 256;
+        return $ipCalc;
+    }
+    
+    function getInfosByIP($ip, $dbConn){
+        $sql = $dbConn->query("SELECT 'country_code', 'country_name', 'region_name', 'city_name', 'latitude', 'longitude' FROM geoip WHERE ip_form=?");
+        $infos = $sql->execute(array($ip));
+        var_dump($infos);
+    }
+    
+    function geolocalisation($dbConn){
+        $cli_ip = getClientIP();
+        $cli_ip = transformIP($cli_ip);
+        getInfosByIP($cli_ip, $dbConn);
+    }
+
 
     //database infos
     $dsn = 'mysql:dbname=geoip;host=127.0.0.1';
@@ -11,18 +32,16 @@
 
     //Conection to the db
     try {
-        $dbh = new PDO($dsn, $user, $password, array(
-            PDO::MYSQL_ATTR_LOCAL_INFILE => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL
-        ));
+        $dbh = new PDO($dsn, $user, $password);
     } catch (PDOException $e) {
         print "Error connect !: " . $e->getMessage() . "<br/>";
         die();
     }
-    
+
+    geolocalisation($dbh);
+
     //Get files content and add value to db
-    $open = fopen("geoip.csv", "r");
+    /*$open = fopen("geoip.csv", "r");
     $line = fgetcsv($open, 1000, ",");
     $i = 0;
 
@@ -53,5 +72,6 @@
         $i++;
     }
     fclose($open);
+    */
 
 ?>
